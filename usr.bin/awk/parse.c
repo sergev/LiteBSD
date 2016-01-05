f3e886213c402f05685b23ce5e1d1ef68edfb9d4
Brian Callahan <dodonpachi-github@mailinator.com>
2016-01-05 08:05:36 -0500

    NetBSD has a pkgsrc version of nawk (it's modern). We can use it. Update to this nawk.

--- usr.bin/awk/parse.c
+++ usr.bin/awk/parse.c
@@ -1,3 +1,5 @@
+/* $NetBSD: parse.c,v 1.1 2006/07/14 14:23:06 jlam Exp $ */
+
 /****************************************************************
 Copyright (C) Lucent Technologies 1997
 All Rights Reserved
@@ -253,7 +255,7 @@ void defn(Cell *v, Node *vl, Node *st)	/* turn on FCN bit in definition, */
 	dprintf( ("defining func %s (%d args)\n", v->nval, n) );
 }
 
-int isarg(char *s)		/* is s in argument list for current function? */
+int isarg(const char *s)		/* is s in argument list for current function? */
 {			/* return -1 if not, otherwise arg # */
 	extern Node *arglist;
 	Node *p = arglist;
