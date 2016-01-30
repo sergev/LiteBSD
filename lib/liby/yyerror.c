cdfdbf4e5fa10f5aa930d6375d0a080334cc78c5
Brian Callahan <dodonpachi-github@mailinator.com>
2016-01-29 20:24:11 -0500

    liby cleanup

--- lib/liby/yyerror.c
+++ lib/liby/yyerror.c
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -31,14 +27,10 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
-static char sccsid[] = "@(#)yyerror.c	8.1 (Berkeley) 6/4/93";
-#endif /* not lint */
-
 #include <stdio.h>
 
-yyerror(msg)
-char *msg;
+int
+yyerror(char *msg)
 {
 	(void)fprintf(stderr, "%s\n", msg);
 	return(0);
