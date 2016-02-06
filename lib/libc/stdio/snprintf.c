1573d35637358603aae84b7edbca76260b5f551d
Brian Callahan <dodonpachi-github@mailinator.com>
2016-02-06 14:12:37 -0500

    Update snprintf(3) to OpenBSD -current version. This fixes some bugs in the previous implementation that showed up trying to update unifdef(1).

--- lib/libc/stdio/snprintf.c
+++ lib/libc/stdio/snprintf.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD: snprintf.c,v 1.19 2015/08/31 02:53:57 guenther Exp $ */
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -13,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -34,43 +31,34 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)snprintf.c	8.1 (Berkeley) 6/4/93";
-#endif /* LIBC_SCCS and not lint */
-
+#include <limits.h>
 #include <stdio.h>
-#if __STDC__
+#include <string.h>
 #include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
 
-#if __STDC__
-snprintf(char *str, size_t n, char const *fmt, ...)
-#else
-snprintf(str, n, fmt, va_alist)
-	char *str;
-	size_t n;
-	char *fmt;
-	va_dcl
-#endif
+int
+snprintf(char *str, size_t n, const char *fmt, ...)
 {
-	int ret;
 	va_list ap;
+	int ret;
+	char dummy;
 	FILE f;
 
-	if ((int)n < 1)
-		return (EOF);
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
+	/* While snprintf(3) specifies size_t stdio uses an int internally */
+	if (n > INT_MAX)
+		n = INT_MAX;
+	/* Stdio internals do not deal correctly with zero length buffer */
+	if (n == 0) {
+		str = &dummy;
+		n = 1;
+	}
+	f._file = -1;
 	f._flags = __SWR | __SSTR;
 	f._bf._base = f._p = (unsigned char *)str;
 	f._bf._size = f._w = n - 1;
+	va_start(ap, fmt);
 	ret = vfprintf(&f, fmt, ap);
-	*f._p = 0;
 	va_end(ap);
+	*f._p = '\0';
 	return (ret);
 }
